30439523f4dd1dc44707301cad03e68f7ff18bef
Franklin Remak <franmak18@gmail.com>
2020-08-08 14:21:43 +0200

    added ban audit log

--- index.js
+++ index.js
@@ -133,6 +133,31 @@ bot.on('guildMemberRemove', async member => {
 		console.log(`${member.user.tag} left the guild, audit log fetch was inconclusive.`);
 	}
 });
+client.on('guildBanAdd', async (guild, user) => {
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'audit-log');
+    if (!channel) return;
+	const fetchedLogs = await guild.fetchAuditLogs({
+		limit: 1,
+		type: 'MEMBER_BAN_ADD',
+	});
+	// Since we only have 1 audit log entry in this collection, we can simply grab the first one
+	const banLog = fetchedLogs.entries.first();
+
+	// Let's perform a coherence check here and make sure we got *something*
+	if (!banLog) return console.log(`${user.tag} was banned from ${guild.name} but no audit log could be found.`);
+
+	// We now grab the user object of the person who banned the user
+	// Let us also grab the target of this action to double check things
+	const { executor, target } = banLog;
+
+	// And now we can update our output with a bit more information
+	// We will also run a check to make sure the log we got was for the same kicked member
+	if (target.id === user.id) {
+		channel.send(`${user.tag} got hit with the swift hammer of justice in the guild ${guild.name}, wielded by the mighty ${executor.tag}`);
+	} else {
+		console.log(`${user.tag} got hit with the swift hammer of justice in the guild ${guild.name}, audit log fetch was inconclusive.`);
+	}
+});
 
 bot.on('message', message => {
     if (!message.content.startsWith(PREFIX) || message.author.bot) return;
